a2e897703b388ad1b43c2e23898cc105cd2662c3
frsbrito <frsbrito@gmail.com>
2022-06-21 18:44:08 -0300

    starting with Go

---
+++ simulador-aluno/go.mod
@@ -0,0 +1,3 @@
+module github.com/frsbrito/Imersao-Full-Stack
+
+go 1.16
---
+++ simulador-aluno/main.go
@@ -0,0 +1,7 @@
+package main
+
+import route2 "github.com/frsbrito/Imersao-Full-Stack/application/route"
+
+func main() {
+	route := route2.Route
+}
